8be585ce96a630a7e32e9f54c06d73da2ec0a190
Sloy <rafa.vazsan@gmail.com>
2011-12-03 22:13:02 +0100

    Corregido NullPointerException con el limpiarExtras

--- src/com/sloy/sevibus/utils/IntentMapa.java
+++ src/com/sloy/sevibus/utils/IntentMapa.java
@@ -6,26 +6,27 @@ import android.content.Intent;
 import com.sloy.sevibus.ui.MapaActivity;
 
 public class IntentMapa extends Intent {
-	
-	public IntentMapa(Context context){
-		super(context,MapaActivity.class);
+
+	public IntentMapa(Context context) {
+		super(context, MapaActivity.class);
 	}
-	
-	public IntentMapa setLinea(long lineaID){
+
+	public IntentMapa setLinea(long lineaID) {
 		limpiarExtras();
 		this.putExtra("linea", lineaID);
 		return this;
 	}
-	
-	public IntentMapa setParada(long l){
+
+	public IntentMapa setParada(long l) {
 		limpiarExtras();
 		this.putExtra("parada", l);
 		return this;
 	}
-	
-	private void limpiarExtras(){
-		this.getExtras().clear();
+
+	private void limpiarExtras() {
+		if(this.getExtras() != null){
+			this.getExtras().clear();
+		}
 	}
-	
 
 }
