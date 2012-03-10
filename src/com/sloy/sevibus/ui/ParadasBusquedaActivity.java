5f0817ddf4f19d958bbabfc64533d0ac516d075f
Sloy <rafa.vazsan@gmail.com>
2012-03-11 00:19:14 +0100

    Pasada búsqueda de parada al ActionBar, y búsqueda automatizada

--- src/com/sloy/sevibus/ui/ParadasBusquedaActivity.java
+++ src/com/sloy/sevibus/ui/ParadasBusquedaActivity.java
@@ -3,8 +3,11 @@ package com.sloy.sevibus.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -15,6 +18,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.android.dataframework.DataFramework;
 import com.android.dataframework.Entity;
@@ -38,18 +43,24 @@ public class ParadasBusquedaActivity extends SherlockActivity {
 		setContentView(R.layout.activity_paradas);
 		getSupportActionBar().setHomeButtonEnabled(true);
 		getSupportActionBar().setDisplayUseLogoEnabled(false);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);getSupportActionBar().setDisplayShowTitleEnabled(false);
+		
+		// Inflate the custom view
+		View customNav = LayoutInflater.from(this).inflate(R.layout.paradas_search_actionbar, null);
+		// Attach to the action bar
+		getSupportActionBar().setCustomView(customNav);
+		getSupportActionBar().setDisplayShowCustomEnabled(true);
 
 		mEmpty = (TextView)findViewById(R.id.paradas_vacio);
 		mList = (ListView)findViewById(android.R.id.list);
-		mTxtBusqueda = (EditText)findViewById(R.id.paradas_busqueda);
-		mBtSearch = (Button)findViewById(R.id.paradas_buscar);
-		mBtSearch.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				buscar();
-			}
-		});
+		mTxtBusqueda = (EditText)customNav.findViewById(R.id.paradas_busqueda);
+//		mBtSearch = (Button)customNav.findViewById(R.id.paradas_buscar);
+//		mBtSearch.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				buscar();
+//			}
+//		});
 
 		mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
@@ -62,38 +73,48 @@ public class ParadasBusquedaActivity extends SherlockActivity {
 			@Override
 			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 				if(actionId == EditorInfo.IME_ACTION_SEARCH){
-					buscar();
+					//botón buscar del teclado
+					buscar(true);
 				}
 				return true;
 			}
 		});
 
-		/*
-		 * mTxtBusqueda.addTextChangedListener(new TextWatcher() {
-		 * @Override
-		 * public void onTextChanged(CharSequence s, int start, int before, int
-		 * count) {
-		 * Log.d("sevibus", "onTextChanged");
-		 * buscar();
-		 * }
-		 * @Override
-		 * public void beforeTextChanged(CharSequence s, int start, int count,
-		 * int after) {
-		 * Log.d("sevibus", "beforeTextChanged");
-		 * }
-		 * @Override
-		 * public void afterTextChanged(Editable s) {
-		 * Log.d("sevibus", "aTextChanged");
-		 * }
-		 * });
-		 */
+		mTxtBusqueda.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+//				Log.d("sevibus", "onTextChanged");
+				// Cuando cambia el texto del cuadro
+				buscar(false);
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//				Log.d("sevibus", "beforeTextChanged");
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+//				Log.d("sevibus", "aTextChanged");
+			}
+		});
+
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getSherlock().getMenuInflater();
+		inflater.inflate(R.menu.paradas_search, menu);
+		return true;
 	}
 
-	private void buscar() {
+	private void buscar(boolean quitarTeclado) {
+		if(quitarTeclado){
 		// quita el teclado
-		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.hideSoftInputFromWindow(mTxtBusqueda.getWindowToken(), 0);
-
+			InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+ imm.hideSoftInputFromWindow(mTxtBusqueda.getWindowToken(), 0);
+		}
 		// extrae la consulta
 		String query = limpiaConsulta(mTxtBusqueda.getText().toString());
 
@@ -142,10 +163,13 @@ public class ParadasBusquedaActivity extends SherlockActivity {
 		}
 		return res;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()){
+			case R.id.menu_buscar:
+				buscar(true);
+				return true;
 			case android.R.id.home:
 				startActivity(new Intent(this, HomeActivity.class));
 				return true;
