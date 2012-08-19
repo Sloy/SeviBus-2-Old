eaed30296a9ccf0ad391930300608e3a81f6b4b5
Sloy <rafa.vazsan@gmail.com>
2012-08-19 14:23:01 +0200

    Botón cutre para descargar la aplicación de SeviBus para Android si se abre desde un dispositivo Android

--- parts/home_header.php
+++ parts/home_header.php
@@ -2,6 +2,17 @@
 	<a href="acerca.php" data-icon="info" data-transition="flip">Acerca de</a>
 	<h1>SeviBus Web (alfa)</h1>
 	<a href="contribuir.php" data-rel="dialog" data-transition="slidedown">Contribuye</a>
+	
+	<?php 
+	if(strstr($_SERVER['HTTP_USER_AGENT'], 'Android')){
+		?>
+		<div class="ui-bar ui-bar-c">
+			<h3>Aplicación para Android <a href="https://play.google.com/store/apps/details?id=com.sloy.sevibus" data-role="button">Descargar</a></h3>
+		</div>
+		<?
+	}
+	?>
+
 	<div data-role="navbar" style="padding:0px">
 		<ul>
 			<li><a href="home_paradas.php" class="ui-btn-active">Paradas</a></li>
@@ -9,4 +20,5 @@
 			<li><a href="home_mapa.php">Mapa</a></li>
 		</ul>
 	</div>
+	
 </div><!-- /header -->
