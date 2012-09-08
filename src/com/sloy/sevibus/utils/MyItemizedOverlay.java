4b84167553b100bd5cdabb99ec02e38944472d6f
Sloy <rafa.vazsan@gmail.com>
2012-09-08 23:53:23 +0200

    Adaptado el ItemizedOverlay a la nueva versión de la librería MapViewBalloons

--- src/com/sloy/sevibus/utils/MyItemizedOverlay.java
+++ src/com/sloy/sevibus/utils/MyItemizedOverlay.java
@@ -3,7 +3,12 @@ package com.sloy.sevibus.utils;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.android.dataframework.Entity;
 import com.google.android.maps.GeoPoint;
@@ -11,6 +16,8 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 import com.google.common.collect.Lists;
 import com.readystatesoftware.mapviewballoons.BalloonItemizedOverlay;
+import com.readystatesoftware.mapviewballoons.BalloonOverlayView;
+import com.readystatesoftware.mapviewballoons.R;
 
 public class MyItemizedOverlay extends BalloonItemizedOverlay<OverlayItem> {
 
@@ -21,9 +28,12 @@ public class MyItemizedOverlay extends BalloonItemizedOverlay<OverlayItem> {
 
 	public MyItemizedOverlay(Drawable defaultMarker, MapView mapView, Context context) {
 		super(boundCenter(defaultMarker), mapView);
+
 		mParadas = Lists.newArrayList();
 		mOverlays = Lists.newArrayList();
 		mContext = context;
+		
+		this.setShowClose(false);
 	}
 
 	public void addOverlay(OverlayItem overlay) {
@@ -85,5 +95,60 @@ public class MyItemizedOverlay extends BalloonItemizedOverlay<OverlayItem> {
 	public void hideBalloon() {
 		super.hideBalloon();
 	}
+
+	@Override
+	protected BalloonOverlayView<OverlayItem> createBalloonOverlayView() {
+		return new CustomBalloonOverlayView(getMapView().getContext(), getBalloonBottomOffset());
+	}
+
+	public class CustomBalloonOverlayView extends BalloonOverlayView<OverlayItem> {
+
+		private TextView title;
+		private TextView snippet;
+		
+		public CustomBalloonOverlayView(Context context, int balloonBottomOffset) {
+			super(context, balloonBottomOffset);
+		}
+		
+		@Override
+		protected void setupView(Context context, final ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = inflater.inflate(R.layout.balloon_overlay, parent);
+			title = (TextView) v.findViewById(R.id.balloon_item_title);
+			snippet = (TextView) v.findViewById(R.id.balloon_item_snippet);
+			
+			title.setTextColor(0xFF990000);
+			title.setTypeface(null, Typeface.BOLD);
+
+		}
+
+		@Override
+		protected void setBalloonData(OverlayItem item, ViewGroup parent) {
+			if (item.getTitle() != null) {
+				title.setVisibility(VISIBLE);
+				title.setText(item.getTitle());
+			} else {
+				title.setText("");
+				title.setVisibility(GONE);
+			}
+			if (item.getSnippet() != null) {
+				snippet.setVisibility(VISIBLE);
+				snippet.setText(item.getSnippet());
+			} else {
+				snippet.setText("");
+				snippet.setVisibility(GONE);
+			}
+		}
+
+	}
+
+	@Override
+	public boolean onTap(GeoPoint arg0, MapView arg1) {
+		hideAllBalloons();
+		return super.onTap(arg0, arg1);
+	}
+	
+	
 
 }
