a6d10fd7901d04f278691a0ed079ba292adeb17a
Sloy <rafa.vazsan@gmail.com>
2012-10-04 16:54:55 -0400

    Añadida información sobre la versión de Android instalada en el reporte

--- src/com/sloy/sevibus/ui/ReporteActivity.java
+++ src/com/sloy/sevibus/ui/ReporteActivity.java
@@ -64,26 +64,38 @@ public class ReporteActivity extends SherlockActivity {
 			mTexto.setError("Un reporte vacío no sirve de nada");
 		} else {
 			// Información extra?
-			if(mDeviceInfo.isChecked()){
-				texto+= "\n\n----------: ";
-				texto+= "\nBrand: "+android.os.Build.BRAND;
-				texto+= "\nDevice: "+android.os.Build.DEVICE;
-				texto+= "\nDisplay: "+android.os.Build.DISPLAY;
-				texto+= "\nHardware: "+android.os.Build.HARDWARE;
-				texto+= "\nManufacturer: "+android.os.Build.MANUFACTURER;
-				texto+= "\nModel: "+android.os.Build.MODEL;
-				texto+= "\nProduct: "+android.os.Build.PRODUCT;
+			if (mDeviceInfo.isChecked()) {
+				texto += "\n\n========== ";
+				texto += "\nInformación del dispositivo";
+				texto += "\n---------- ";
+				texto += "\n- Release: " + android.os.Build.VERSION.RELEASE;
+				texto += "\n- SDK: " + android.os.Build.VERSION.SDK_INT;
+				texto += "\n- Codename: " + android.os.Build.VERSION.CODENAME;
+				texto += "\n- Incremental: "
+						+ android.os.Build.VERSION.INCREMENTAL;
+				texto += "\n- Brand: " + android.os.Build.BRAND;
+				texto += "\n- Device: " + android.os.Build.DEVICE;
+				texto += "\n- Display: " + android.os.Build.DISPLAY;
+				texto += "\n- Hardware: " + android.os.Build.HARDWARE;
+				texto += "\n- Manufacturer: " + android.os.Build.MANUFACTURER;
+				texto += "\n- Model: " + android.os.Build.MODEL;
+				texto += "\n- Product: " + android.os.Build.PRODUCT;
 			}
 			mTexto.setError(null);
 			Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
 			emailIntent.setType("plain/text");
-			emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { getString(R.string.email_address) });
+			emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+					new String[] { getString(R.string.email_address) });
 			emailIntent.putExtra(
 					android.content.Intent.EXTRA_SUBJECT,
-					getString(R.string.email_subject) + " - "
-							+ getResources().getStringArray(R.array.reporte_asuntos)[mAsunto.getSelectedItemPosition()]);
+					getString(R.string.email_subject)
+							+ " - "
+							+ getResources().getStringArray(
+									R.array.reporte_asuntos)[mAsunto
+									.getSelectedItemPosition()]);
 			emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, texto);
-			startActivity(Intent.createChooser(emailIntent, getString(R.string.email_intent)));
+			startActivity(Intent.createChooser(emailIntent,
+					getString(R.string.email_intent)));
 		}
 
 	}
